62116ac4263e31e2fb8c86e6c3a170553539d380
Grian Gajila <griangajila2@gmail.com>
2024-10-01 22:37:09 +0800

    chore: update hero page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,7 +16,7 @@ export const Hero = () => {
 	// };
 	return (
 		<div className="max-w-[1280px] p-4 pt-16">
-			<Container className="flex flex-wrap sm:text-center md:text-center">
+			<Container className="flex flex-wrap text-center lg:text-left">
 				<div className="flex items-center w-full lg:w-1/2">
 					<div className="max-w-2xl mb-8">
 						<h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
@@ -28,7 +28,7 @@ export const Hero = () => {
 							checking, without going to the health center.
 						</p>
 
-						<div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row sm:justify-center md:justify-center">
+						<div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row justify-center lg:justify-start">
 							<a
 								href="https://web3templates.com/templates/nextly-landing-page-template-for-startups"
 								target="_blank"
