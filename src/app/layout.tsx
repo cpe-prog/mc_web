d886aa9e2261bb347a9ce488e73864c60ec5a674
Grian Gajila <griangajila2@gmail.com>
2024-10-05 16:04:04 +0800

    wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
+import Footer from "@/components/Navbar";
 import type { Metadata } from "next";
-import Footer from "../components/Footer";
 import NavBar from "../components/Navbar";
 import "./globals.css";
 import { Providers } from "./providers";
